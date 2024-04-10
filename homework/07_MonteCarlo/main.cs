27aca1ee906a750bbad4b179ac73e7a0893d5463
Tami <tami-cayen@web.de>
2024-04-10 12:11:13 +0200

    mc task b halton

--- homework/07_MonteCarlo/main.cs
+++ homework/07_MonteCarlo/main.cs
@@ -17,61 +17,62 @@ public class main{
         var outstream = new System.IO.StreamWriter("Out.circle_pseudo.data", append:false);
         outstream.WriteLine("N pseudo_result pseudo_error_estimate pseudo_error_actual");
         Func<vector, double> circle = (vector x) => {if(x.norm()<=1) return 1; else return 0;};
-        vector a = new vector(-1.0, -1.0); //lower integration boundary
-        vector b = new vector(1.0, 1.0) ; //upper integration boundary
+        vector a_circ = new vector(-1.0, -1.0); //lower integration boundary
+        vector b_circ = new vector(1.0, 1.0) ; //upper integration boundary
 
         int[] Ns = new int[100];
-
         for (int i = 50, j=0; i <= 50000; i += 500, j++) Ns[j] = i;
 
         double[] result = new double[Ns.Length];
         double[] sigma = new double[Ns.Length];
         double pseudo_res = 0, pseudo_sig = 0;
         for(int i=0; i<Ns.Length; i++){
-            (pseudo_res, pseudo_sig) = montecarlo.pseudo_plainmc(circle, a, b, Ns[i]);
+            (pseudo_res, pseudo_sig) = montecarlo.pseudo_plainmc(circle, a_circ, b_circ, Ns[i]);
             result[i] = pseudo_res;
             sigma[i] = pseudo_sig;      
             outstream.WriteLine($"{Ns[i]} {result[i]} {sigma[i]} {Abs(PI-result[i])}");
         }
-
-        (pseudo_res, pseudo_sig) = montecarlo.pseudo_plainmc(circle, a, b, 10000);
-        WriteLine($"area of unit circle calculated with pseudo-random mc (10000 sampling points): result={pseudo_res}, estimated error={pseudo_sig}, analytic: π");
+        WriteLine($"area of unit circle calculated with pseudo-random mc ({Ns[Ns.Length-1]} sampling points): result={pseudo_res}, estimated error={pseudo_sig}, analytic: π");
         WriteLine();
 
         /* Calculate area of an ellipse*/
         var outstream2 = new System.IO.StreamWriter("Out.ellipse_pseudo.data", append:false);
         outstream2.WriteLine("N pseudo_result pseudo_error_estimate pseudo_error_actual");
         Func<vector, double> ellipse = (vector x) => {if(x[0]*x[0]/(2*2)+x[1]*x[1]<=1) return 1; else return 0;}; //ellipse with diameter 4 and 2
-        a = new vector(-2.0, -1.0); //lower integration boundary
-        b = new vector(2.0, 1.0) ; //upper integration boundary
-
-        Ns = new int[100];
-        for (int i = 50, j=0; i <= 50000; i += 500, j++) Ns[j] = i;
-        double[] result2 = new double[Ns.Length];
-        double[] sigma2 = new double[Ns.Length];
+        vector a_ell = new vector(-2.0, -1.0); //lower integration boundary
+        vector b_ell = new vector(2.0, 1.0) ; //upper integration boundary
         for(int i=0; i<Ns.Length; i++){
-            (pseudo_res, pseudo_sig) = montecarlo.pseudo_plainmc(ellipse, a, b, Ns[i]);
-            result2[i] = pseudo_res;
-            sigma2[i] = pseudo_sig;      
-            outstream2.WriteLine($"{Ns[i]} {result2[i]} {sigma2[i]} {Abs(2*PI-result2[i])}");
+            (pseudo_res, pseudo_sig) = montecarlo.pseudo_plainmc(ellipse, a_ell, b_ell, Ns[i]);
+            outstream2.WriteLine($"{Ns[i]} {pseudo_res} {pseudo_sig} {Abs(2*PI-pseudo_res)}");
         }
-
-        (pseudo_res, pseudo_sig) = montecarlo.pseudo_plainmc(ellipse, a, b, 10000);
-        WriteLine($"area of ellipse with semi-major and semi-minor axis of length 2 and 1, calculated with pseudo-random mc (10000 sampling points): result={pseudo_res}, estimated error={pseudo_sig}, analytic: 2π");
+        WriteLine($"area of ellipse with semi-major and semi-minor axis of length 2 and 1, calculated with pseudo-random mc ({Ns[Ns.Length-1]} sampling points): result={pseudo_res}, estimated error={pseudo_sig}, analytic: 2π");
         WriteLine();
 
         /*3d integral*/
         Func<vector, double> funcA = (vector x) => {return 1.0/(1.0-Cos(x[0])*Cos(x[1])*Cos(x[2]))/(Pow(PI, 3));}; 
-        a = new vector(0.0, 0.0, 0.0); //lower integration boundary
-        b = new vector(PI, PI, PI) ; //upper integration boundary
-
-        (pseudo_res, pseudo_sig) = montecarlo.pseudo_plainmc(funcA, a, b, 500000);
-        WriteLine($" ∫0π  dx/π ∫0π  dy/π ∫0π  dz/π [1-cos(x)cos(y)cos(z)]^-1 = Γ(1/4)4/(4π3) calculated with pseudo-random mc (50000 sampling points): result={pseudo_res}, estimated error={pseudo_sig}, analytic:{Pow(3.62561, 4)/(4*PI*PI*PI) : 0.000000} ");
-
-
+        vector a_3d = new vector(0.0, 0.0, 0.0); //lower integration boundary
+        vector b_3d = new vector(PI, PI, PI) ; //upper integration boundary
+        (pseudo_res, pseudo_sig) = montecarlo.pseudo_plainmc(funcA, a_3d, b_3d, 500000);
+        WriteLine($"∫0π  dx/π ∫0π  dy/π ∫0π  dz/π [1-cos(x)cos(y)cos(z)]^-1 = Γ(1/4)4/(4π3) calculated with pseudo-random mc (50000 sampling points): result={pseudo_res}, estimated error={pseudo_sig}, analytic:{Pow(3.62561, 4)/(4*PI*PI*PI) : 0.000000} ");
+        WriteLine();
 
         outstream.Close();
         outstream2.Close();
+        
+        WriteLine("---Task B-------");
+        /* Calculate area of a circle using halton series*/
+        var outstream_quasi = new System.IO.StreamWriter("Out.circle_quasi.data", append:false);
+        outstream_quasi.WriteLine("N quasi_result quasi_error_estimate quasi_error_actual");
+        double quasi_res = 0, quasi_sig = 0;
+        for(int i=0; i<Ns.Length; i++){
+            (quasi_res, quasi_sig) = montecarlo.quasi_mc(circle, a_circ, b_circ, Ns[i]);
+            outstream_quasi.WriteLine($"{Ns[i]} {quasi_res} {quasi_sig} {Abs(PI-quasi_res)}");
+        }
+        WriteLine($"area of unit circle calculated with quasi-random mc ({Ns[Ns.Length-1]} sampling points): result={quasi_res}, estimated error={quasi_sig}, analytic: π");
+        WriteLine();
+
+        outstream_quasi.Close();
+
         return 0;
     }
 }
--- homework/07_MonteCarlo/montecarlo.cs
+++ homework/07_MonteCarlo/montecarlo.cs
@@ -20,4 +20,52 @@ public static (double,double) pseudo_plainmc(Func<vector,double> f,vector a,vect
     return result;
 }
 
+public static (double, double) quasi_mc(Func<vector,double> f,vector a,vector b,int N){
+    int dim=a.size;
+    double V=1;
+    for(int i=0;i<dim;i++)V*=b[i]-a[i];
+    double sum_a=0,sum_b=0;
+	vector x=new vector(dim);
+    vector qrnd;
+    for(int j = 0; j<N; j++){ //calculation with a halton sequence
+        qrnd = halton(j, dim); 
+        for(int k=0;k<dim;k++)x[k]=a[k]+qrnd[k]*(b[k]-a[k]);
+        double fx=f(x);
+        sum_a+=fx;
+    }
+
+    for(int j = 0; j<N; j++){ //calculation with ANOTHER halton sequence
+        qrnd = halton(j, dim, 1); //produces another halton series by shifting the base by 1 
+        for(int k=0;k<dim;k++)x[k]=a[k]+qrnd[k]*(b[k]-a[k]);
+        double fx=f(x);
+        sum_b+=fx;
+    }
+
+    double mean_a=sum_a/N;
+    double mean_b=sum_b/N;
+    double sigma= Abs(mean_a - mean_b);
+    var result=(mean_a*V,sigma*V);
+    return result;
+}
+
+public static double corput ( int n , int b){
+    double q=0, bk=(double)1.0/b ;
+    while(n>0){
+        q += (n % b)*bk ;
+        n /= b ;
+        bk /= b ;
+    }
+    return q ;
+}
+
+public static vector halton(int n , int d, int baseshift = 0){ // returns the n-th Halton d-dimentional point in the unit volume
+    vector x = new vector(d);
+    int[] bas = {2 ,3 ,5 ,7 ,11 ,13 ,17 ,19 ,23 ,29 ,31 ,37 ,41 ,43 ,47 ,53 ,59 ,61};
+    int maxd = bas.Length;
+    if(d > maxd + baseshift){throw new System.Exception("halton dimension exceeds limit");}
+    for(int i =0; i<d; i++) x[i]=corput(n, bas[i + baseshift]) ;
+    return x;
+}
+
+
 }
