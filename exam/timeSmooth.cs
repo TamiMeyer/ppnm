d903be2f4f989173e14cd4d0b997c3da5f719902
Tami <tami-cayen@web.de>
2024-06-23 13:26:50 +0200

    exam c.3 explanation and conclusion

--- exam/timeSmooth.cs
+++ exam/timeSmooth.cs
@@ -19,7 +19,7 @@ public class main{
         //Read the raw noisy signal data from the file
         /*We do not only read N data points but all the data points from the file in order to achieve a constant offset in time,
         otherwise there would be a contribution from the reading which depends on N */
-        List<double> x_raw_list = new List<double>{};//(not required for timing)
+        ///List<double> x_raw_list = new List<double>{};//(not required for timing, but for debugging)
         List<double> y_raw_list = new List<double>{};
         var instream=new System.IO.StreamReader(data);
         instream.ReadLine(); //skip the first line (it contains the title)
@@ -28,7 +28,7 @@ public class main{
         int linenr =0;
         for(string line=instream.ReadLine(); line!=null && linenr<maxN; line=instream.ReadLine()){  
 	        var numbers = line.Split(split_delimiters,split_options);
-            x_raw_list.Add(double.Parse(numbers[0]));//(not required for timing)
+            ///x_raw_list.Add(double.Parse(numbers[0]));//(not required for timing, but for debugging)
             y_raw_list.Add(double.Parse(numbers[2]));
             linenr++;
         }
@@ -38,14 +38,15 @@ public class main{
         int signalCount = y_raw_list.Count;
         if(signalCount < N ) throw new ArgumentException("The noisy data set contains less points than N");
         if (signalCount > N) {
-            x_raw_list.RemoveRange(N, signalCount - N);//(not required for timing)
+            ///x_raw_list.RemoveRange(N, signalCount - N);//(not required for timing, but for debugging)
             y_raw_list.RemoveRange(N, signalCount - N);
         }
-        double[] x_raw = x_raw_list.ToArray();//(not required for timing)
+        ///double[] x_raw = x_raw_list.ToArray();//(not required for timing, but for debugging)
         double[] y_raw = y_raw_list.ToArray();
 
         //smoothing with the chosen method (QR or efficientLU)
         vector y_raw_vec = new vector(y_raw);
+        #pragma warning disable CS0219
         vector y_smooth;
         if(String.Equals(method, "QR")){
             y_smooth = smooth.smoothQR(y_raw_vec, lambda);
@@ -62,7 +63,7 @@ public class main{
                     outstream.Close();*/
 
         }
-
+        #pragma warning restore CS0219
 
 
     }
